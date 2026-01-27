b6512d87136bf9e643c7fa79cfc76d22a40a1575
Vinit <canikissyoumylove@gmail.com>
2026-01-27 15:02:20 +0530

    Mongodb model updated

--- minimalApi/Models/Video.cs
+++ minimalApi/Models/Video.cs
@@ -6,7 +6,7 @@ public class Video
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Id { get; set; } = string.Empty;
 
     [BsonElement("title")]
     public string Title { get; set; } = null!;
@@ -25,4 +25,35 @@ public class Video
 
     [BsonElement("videoPath")]
     public string VideoPath { get; set; } = null!;
+
+    [BsonElement("previewPath")]
+    public string previewPath { get; set; } = null!;
+
+    [BsonElement("preview")]
+    public VideoPreview preview { get; set; } = null!;
+}
+
+[BsonIgnoreExtraElements]
+public class VideoPreview
+{
+    [BsonElement("spriteBaseUrl")]
+    public string SpriteBaseUrl { get; set; } = null!;
+
+    [BsonElement("frameInterval")]
+    public int frameInterval { get; set; }
+
+    [BsonElement("spriteCount")]
+    public int spriteCount { get; set; }
+
+    [BsonElement("cols")]
+    public int cols { get; set; }
+
+    [BsonElement("rows")]
+    public int rows { get; set; }
+
+    [BsonElement("frameWidth")]
+    public int frameWidth { get; set; }
+
+    [BsonElement("frameHeight")]
+    public int frameHeight { get; set; }
 }
